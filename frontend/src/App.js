b414da9c69b022c5c446dd27a64bcde592c57dcc
CaiqueSampaio <36681822+CaiqueSam@users.noreply.github.com>
2020-03-29 14:46:33 -0300

    Frontend da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import Header from './Header';
-
+import React/*, { useState }*/ from 'react';
+//import Logon from './pages/Logon';
+import './global.css';
+import Routes from './routes';
 
 function App() {
+  /*const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <Header />
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
+    
+  );*/
+  return (
+    <Routes />
   );
 }
 
